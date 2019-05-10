0297a0d72a33251537085f64e674291724d55657
gregoryFine <caoxiaolu2007@163.com>
2019-05-10 10:03:01 +0800

    add log

--- sboot_rabbit_producer/src/main/java/com/ln/producer/OrderSender.java
+++ sboot_rabbit_producer/src/main/java/com/ln/producer/OrderSender.java
@@ -1,5 +1,7 @@
 package com.ln.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import com.ln.entity.Order;
 @Component
 public class OrderSender {
 
+	Logger logger = LoggerFactory.getLogger(OrderSender.class);
+	
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 
@@ -17,11 +21,12 @@ public class OrderSender {
 
 		CorrelationData correlationData = new CorrelationData();
 		correlationData.setId(order.getMessageId());
-
+		
 		rabbitTemplate.convertAndSend("order-exchange",//exchange
 				"order.abcd",//routing-key
 				order,//消息內容
 				correlationData);//消息唯一id
+		logger.info("------------------------生产一条消息 ---------------------------");
 	}
 
 }
